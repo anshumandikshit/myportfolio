9c8d90ba391598c8b479dd0c8882f78d62b472ad
anshumandikshit <anshuman.dikshit@gmail.com>
2025-01-05 22:21:18 +0530

    Add skills section with Progress Bar animations

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("JS is running ") ;
+const navbar = document.getElementById('navBar') ;
+const skilsItems = document.querySelectorAll('.skills__card__item') ;
+
+// console.log(skilsItems) ;
+
+const onNavBarClick = (event)=>{
+    // console.log(event) ;
+    if(event.target.classList.contains('navItem')){
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({
+            behavior : "smooth"
+        }) ;
+    }
+}
+
+skilsItems.forEach(item=>{
+    const skillVal = item.querySelector('.item__progressNumber') ;
+    let val = skillVal.innerHTML.trim() ;
+    // console.log("skillVal",skillVal) ;
+    // console.log("skillValContent",skillVal.innerHTML.trim())
+    const progressBar = item.querySelector('.progress-bar') ;
+    progressBar.style.width = val ;
+})
+
+navbar.addEventListener("click",onNavBarClick);
